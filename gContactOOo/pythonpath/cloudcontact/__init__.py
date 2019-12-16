810cbeee2959518906a4655890aa223f0d8e8d96
User <prrvchr@gmail.com>
2019-12-16 18:42:42 +0100

    pythonpath Fix

--- gContactOOo/pythonpath/cloudcontact/__init__.py
+++ gContactOOo/pythonpath/cloudcontact/__init__.py
@@ -4,3 +4,11 @@
 from .configuration import g_identifier
 from .configuration import g_host
 from .configuration import g_url
+
+from .dbinit import getDataSourceUrl
+from .dbtools import getDataSourceConnection
+from .dbtools import getDataBaseInfo
+
+from .user import User
+from .datasource import DataSource
+from .connection import Connection
