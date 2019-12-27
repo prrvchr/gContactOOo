29a303e56479f4f020049c16492cd8add0b379a4
User <prrvchr@gmail.com>
2019-12-27 22:57:59 +0100

    configuration.py Fix

--- gContactOOo/pythonpath/cloudcontact/configuration.py
+++ gContactOOo/pythonpath/cloudcontact/configuration.py
@@ -4,9 +4,12 @@
 # General configuration
 g_extension = 'gContactOOo'
 g_identifier = 'com.gmail.prrvchr.extensions.%s' % g_extension
+g_logger = '%s.Logger' % g_identifier
+
 g_host = 'people.googleapis.com'
 g_version = 'v1'
 g_url = 'https://%s/%s' % (g_host, g_version)
+
 g_timestamp = '%Y-%m-%dT%H:%M:%S.00'
 g_IdentifierRange = (10, 50)
 g_userfields = 'userDefined'
