52b70d105bfcae9997782600ae23f332625a20d9
Pierre <prrvchr@gmail.com>
2022-12-20 14:44:02 +0100

    uno subrepo update

--- uno/lib/uno/addressbook/database.py
+++ uno/lib/uno/addressbook/database.py
@@ -109,10 +109,7 @@ class DataBase(unohelper.Base,
             connection = getDataSourceConnection(ctx, self._url)
             error = self._createDataBase(connection)
             if error is None:
-                datasource = connection.getParent()
-                #datasource.SuppressVersionColumns = True
-                datasource.DatabaseDocument.storeAsURL(odb, ())
-                datasource.dispose()
+                connection.getParent().DatabaseDocument.storeAsURL(odb, ())
             connection.close()
         print("gContact.DataBase.init() end")
 
