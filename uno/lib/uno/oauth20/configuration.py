eaf8c00c0affbbca62154c0b1b167e21e34c9be7
prrvchr <prrvchr@gmail.com>
2025-01-15 16:27:30 +0100

    git subrepo pull (merge) uno
    subrepo:
      subdir:   "uno"
      merged:   "59189b14"
    upstream:
      origin:   "https://github.com/prrvchr/uno.git"
      branch:   "main"
      commit:   "67fb6f3f"
    git-subrepo:
      version:  "0.4.3"
      origin:   "https://github.com/ingydotnet/git-subrepo.git"
      commit:   "2f68596"

--- uno/lib/uno/oauth20/configuration.py
+++ uno/lib/uno/oauth20/configuration.py
@@ -27,83 +27,21 @@
 ╚════════════════════════════════════════════════════════════════════════════════════╝
 """
 
-import uno
-import unohelper
+# OAuth2 configuration
+g_extension = 'OAuth2OOo'
+g_identifier = 'io.github.prrvchr.%s' % g_extension
+g_service = '%s.OAuth2Service' % g_identifier
+g_resource = 'resource'
 
-from com.sun.star.task import XInteractionRequest
-from com.sun.star.task import XInteractionAbort
-from com.sun.star.auth import XInteractionUserName
-from com.sun.star.auth import OAuth2Request
+g_version = '1.4.0'
+g_chunk = g_chunk = 320 * 1024
 
+g_token = 'Bearer ${AccessToken}'
 
-# Wrapper to make callable OAuth2Service
-class NoOAuth2(object):
-    def __call__(self, request):
-        return request
+g_basename = 'OAuth2'
+g_defaultlog = 'OAuth2Logger'
+g_errorlog = 'OAuth2Error'
 
-
-# Wrapper to make callable OAuth2Service
-class OAuth2OOo(NoOAuth2):
-    def __init__(self, oauth2):
-        self._oauth2 = oauth2
-
-    def __call__(self, request):
-        request.headers['Authorization'] = self._oauth2.getToken('Bearer %s')
-        return request
-
-
-class InteractionAbort(unohelper.Base,
-                       XInteractionAbort):
-
-    # XInteractionAbort
-    def select(self):
-        pass
-
-
-class InteractionUserName(unohelper.Base,
-                          XInteractionUserName):
-    def __init__(self):
-        self._username = ''
-        self._token = ''
-
-    # XInteractionUserName
-    def setUserName(self, name):
-        self._username = name
-
-    def getUserName(self):
-        return self._username
-
-    def setToken(self, token):
-        self._token = token
-
-    def getToken(self):
-        return self._token
-
-    def select(self):
-        pass
-
-
-class InteractionRequest(unohelper.Base,
-                         XInteractionRequest):
-    def __init__(self, source, url, user, format, message):
-        self._request = self._getRequest(source, url, user, format, message)
-        self._abort = InteractionAbort()
-        self._continue = InteractionUserName()
-
-    # XInteractionRequest
-    def getRequest(self):
-        return self._request
-
-    def getContinuations(self):
-        return (self._abort, self._continue)
-
-    def _getRequest(self, context, url, user, format, message):
-        request = OAuth2Request()
-        classification = 'com.sun.star.task.InteractionClassification'
-        request.Classification = uno.Enum(classification, 'QUERY')
-        request.Context = context
-        request.ResourceUrl = url
-        request.UserName = user
-        request.Format = format
-        request.Message = message
-        return request
+g_wizard_page = 2 # -1 to disable
+g_wizard_paths = ((1, 2, 3, 4), (1, 4))
+g_refresh_overlap = 10 # must be positive, in second
