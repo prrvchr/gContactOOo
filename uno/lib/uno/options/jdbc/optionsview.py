dec1eb3c60e5c1e7651718865afc7f0fc17a90ca
prrvchr <prrvchr@gmail.com>
2025-10-14 10:38:09 +0200

    git subrepo pull uno
    subrepo:
      subdir:   "uno"
      merged:   "e55f7380"
    upstream:
      origin:   "https://github.com/prrvchr/uno.git"
      branch:   "main"
      commit:   "e55f7380"
    git-subrepo:
      version:  "0.4.9"
      origin:   "https://github.com/ingydotnet/git-subrepo.git"
      commit:   "4f60dd7"

--- uno/lib/uno/options/jdbc/optionsview.py
+++ uno/lib/uno/options/jdbc/optionsview.py
@@ -27,73 +27,43 @@
 ╚════════════════════════════════════════════════════════════════════════════════════╝
 """
 
-from com.sun.star.logging.LogLevel import INFO
+from ...unotool import getContainerWindow
 
-from ..unotool import getConfiguration
-
-from ..logger import getLogger
-
-from ..jdbcdriver import g_services
-
-from ..configuration import g_identifier
-from ..configuration import g_basename
+from ...configuration import g_identifier
 
 import traceback
 
 
-class OptionModel():
-    def __init__(self, ctx, instrumented):
-        self._rebootkeys = ('ApiLevel', 'CachedRowSet')
-        configkeys = ('ShowSystemTable', )
-        self._keys = self._rebootkeys + configkeys
-        self._config = getConfiguration(ctx, g_identifier, True)
-        self._settings = self._getSettings()
-        self._instrumented = instrumented
+class OptionsWindow():
+    def __init__(self, ctx, window, handler, options):
+        self._window = getContainerWindow(ctx, window.getPeer(), handler, g_identifier, 'OptionDialog')
+        self._window.setVisible(True)
+        self.enableCachedRowSet(False, options)
 
-# OptionModel getter methods
-    def isInstrumented(self):
-        return self._instrumented
+# OptionWindow setter methods
+    def dispose(self):
+        self._window.dispose()
 
-    def getConfigApiLevel(self):
-        return self._config.getByName('ApiLevel')
+    def initView(self, instrumented, level, crs, system, enabled):
+        self._getApiLevel(level).State = 1
+        if instrumented:
+            self._getCachedRowSet(crs).State = 1
+        else:
+            self._getCachedRowSet(0).State = 1
+        self.enableCachedRowSet(instrumented and enabled)
+        self._getSytemTable().State = int(system)
 
-    def getViewData(self):
-        self._settings = self._getSettings()
-        level = self._settings.get('ApiLevel')
-        crs = self._settings.get('CachedRowSet')
-        system = self._settings.get('ShowSystemTable')
-        return level, crs, system, self._isRowSetEnabled(level)
+    def enableCachedRowSet(self, enabled, options=(0, 1, 2)):
+        for index in options:
+            self._getCachedRowSet(index).Model.Enabled = enabled
 
-# OptionModel setter methods
-    def setApiLevel(self, level):
-        self._settings['ApiLevel'] = level
-        return self._instrumented and self._isRowSetEnabled(level)
+# OptionWindow private control methods
+    def _getApiLevel(self, index):
+        return self._window.getControl('OptionButton%s' % (index + 1))
 
-    def setCachedRowSet(self, level):
-        self._settings['CachedRowSet'] = level
+    def _getCachedRowSet(self, index):
+        return self._window.getControl('OptionButton%s' % (index + 4))
 
-    def setSystemTable(self, state):
-        self._settings['ShowSystemTable'] = bool(state)
+    def _getSytemTable(self):
+        return self._window.getControl('CheckBox1')
 
-    def saveSetting(self):
-        reboot = False
-        for key in self._keys:
-            if key != 'CachedRowSet' or self._instrumented:
-                value = self._settings.get(key)
-                if value != self._config.getByName(key):
-                    self._config.replaceByName(key, value)
-                    if key in self._rebootkeys:
-                        reboot = True
-        if self._config.hasPendingChanges():
-            self._config.commitChanges()
-        return reboot
-
-# OptionModel private methods
-    def _getSettings(self):
-        settings = {}
-        for key in self._keys:
-            settings[key] = self._config.getByName(key)
-        return settings
-
-    def _isRowSetEnabled(self, level):
-        return level != 0
